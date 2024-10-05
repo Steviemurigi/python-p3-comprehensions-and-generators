26b66a7f6962e4b5111c8502e96c1ff72d6d2273
Steve Murigi <steve.murigi@student.moringaschool.com>
2024-10-04 23:56:59 -0500

    lab done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    evens = [n for n in num_list if n % 2 == 0]
+    return evens
     pass
 
 def make_exclamation(sentence_list):
+    return [s + "!" for s in sentence_list]
     pass
